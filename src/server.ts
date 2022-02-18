62cd97ad4a1f710007590ce854ed41bc8727c6f6
Ton Titor <44329784+tontitor@users.noreply.github.com>
2022-02-18 14:18:03 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { response } from "express";
 
-// @types/express // biblioteca de tipagem
 
 const app = express();
-
+ 
 // http://localhost:3000
+
+app.get("/test", (resquest, response) => {
+  return response.send("Testando");
+});
+
+app.post("/test-post", (resquest, response) => {
+  return response.send("Olá método POST");
+});
 
 app.listen(3000, () => console.log("Server is running"));
