606b38a0321cb95e23b9cf479483e778684b13ef
Ton Titor <44329784+tontitor@users.noreply.github.com>
2022-02-18 09:54:36 -0300

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+// @types/express // biblioteca de tipagem
+
+const app = express();
+
+// http://localhost:3000
+
+app.listen(3000, () => console.log("Server is running"));
