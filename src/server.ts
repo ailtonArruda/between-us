46d6f19659b01548ba8fc36e7de5c0874c6c911a
Ton Titor <44329784+tontitor@users.noreply.github.com>
2022-02-21 18:03:08 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
-import express, { response } from "express";
-
+import express from "express";
+import "reflect-metadata";
+import "./database";
 
 const app = express();
- 
-// http://localhost:3000
-
-app.get("/test", (resquest, response) => {
-  return response.send("Testando");
-});
-
-app.post("/test-post", (resquest, response) => {
-  return response.send("Olá método POST");
-});
 
 app.listen(3000, () => console.log("Server is running"));
