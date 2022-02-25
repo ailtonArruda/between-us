700b0ea684da1f8a2bd9725c99a315eb368a0fb9
Ton Titor <44329784+tontitor@users.noreply.github.com>
2022-02-25 13:08:54 -0300

    Add files via upload

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+
+class CreateUserService{
+
+  static execute(arg0: { name: any; email: any; admin: any; }) {
+    throw new Error("Method not implemented.");
+  }
+
+  async execute({ name , email, admin } : IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepositories);
+
+    if(!email) {
+      throw new Error("Email incorrect");
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");      
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
