a339fb12afbdfd47b1e78cac3abc022f92c3f0c9
Ton Titor <44329784+tontitor@users.noreply.github.com>
2022-03-04 17:53:55 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,17 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CretateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CretateTagController();
+const authenticateUserController = new AuthenticateUserController;
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/login", authenticateUserController.handle);
 
 export { router };
