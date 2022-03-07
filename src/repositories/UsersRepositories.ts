301f7f93c86e87ebf3da553da26ef4cb908e2204
Ton Titor <44329784+tontitor@users.noreply.github.com>
2022-03-07 11:45:05 -0300

    Update UsersRepositories.ts

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -3,6 +3,10 @@ import { User } from "../entities/User";
 
 
 @EntityRepository(User)
-class UsersRepositories extends Repository<User> {}
+class UsersRepositories extends Repository<User> {
+  static findOne(arg0: { email: string; }) {
+    throw new Error("Method not implemented.");
+  }
+}
 
 export { UsersRepositories };
